28b913d1a04b620e7c1e21d922474e8394ca64e7
liuxin <lx13151052281@gmail.com>
2024-09-05 15:25:57 +0800

    修改

--- ct511.h
+++ ct511.h
@@ -4,12 +4,23 @@
 #include "stdio.h"
 #include "usart.h"
 
-//发送at命令到传感器
-void init_ct511(void);
-//发送数据到对应的mqtt服务器
-void sendMqtt(char *topic,char *message);
 //设置ATCONFIG的连接参数 分别对应 设备名称(随意)  设备账号  设备密码
 void setATCONFIG(char *clientId,char *account ,char* password);
-extern char ct511Data[];
 
+//设置连接的mqtt网址以及端口好
+void setUrlAndPort(char *url,unsigned int port);
+
+//发送at命令到传感器 需要先设置参数
+void init_ct511(void);
+
+void quickUsart(UART_HandleTypeDef* h,char*message);
+
+//连接后 设置订阅命令
+void setATSUB( char *topic) ;
+
+//发送数据至Mqtt服务器
+void sendMqtt(char *topic,char *message);
+
+
+extern char ct511Data[];
 #endif
